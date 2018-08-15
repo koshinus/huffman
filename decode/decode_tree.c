8f6dd792b3be91d18f705994c8a54c968d0fd887
k0shinus <vergile@yandex.ru>
2018-08-16 02:11:59 +0500

    Almost working buffered IO for decoding.

--- decode/decode_tree.c
+++ decode/decode_tree.c
@@ -15,18 +15,40 @@ huffman_decode_node make_decode_node(int16_t left, int16_t right, char symb)
 
 huffman_decode_tree * make_decode_tree(FILE *f)
 {
-    uint16_t nodes_number;
-    char c;
-    fscanf(f, "%"SCNd16"%c", &nodes_number, &c);
-    size_t size = offsetof(huffman_decode_tree, tree) + sizeof(huffman_decode_node) * nodes_number;
-    huffman_decode_tree *hdt = (huffman_decode_tree *) malloc(size);
-    hdt->nodes_number = nodes_number;
-    for(size_t i = 0; i < nodes_number; i++)
+    int16_t nodes_number, nodes_handled = 0, left, right;
+    char symbol;
+    int8_t first_read = 1, decode_node_size = sizeof(huffman_decode_node);
+    huffman_decode_tree *hdt;
+    uint64_t i = 0;
+    while (1)
     {
-        char symbol;
-        int16_t left, right;
-        fscanf(f, "%c%"SCNd16"%"SCNd16"%c", &symbol, &left, &right, &c);
-        hdt->tree[i] = make_decode_node(left, right, symbol);
+        in_bytes_count = fread(in_buffer + i, sizeof(int8_t), IN_BUFFER_SIZE - i, f);
+        i = 0;
+        if(first_read)
+        {
+            nodes_number = ((uint16_t *)in_buffer)[0];
+            i = sizeof(nodes_number);
+            size_t size = offsetof(huffman_decode_tree, tree) + sizeof(huffman_decode_node) * nodes_number;
+            hdt = (huffman_decode_tree *) malloc(size);
+            hdt->nodes_number = nodes_number;
+            first_read = 0;
+        }
+        while(1)
+        {
+            symbol = in_buffer[i];
+            left = ((uint16_t *)(in_buffer + i + 1))[0];
+            right = ((uint16_t *)(in_buffer + i + 1))[1];
+            i += decode_node_size;
+            hdt->tree[nodes_handled++] = make_decode_node(left, right, symbol);
+            if(nodes_handled == nodes_number)
+                return hdt;
+            if(in_bytes_count - i < decode_node_size)
+            {
+                for(uint64_t j = 0; j < in_bytes_count - i; j++)
+                    in_buffer[j] = in_buffer[i + j];
+                i = in_bytes_count - i;
+                break;
+            }
+        }
     }
-    return hdt;
 }
--- decode/support_decode_steps.c
+++ decode/support_decode_steps.c
@@ -9,19 +9,37 @@ void decode(huffman_decode_tree *hdt, FILE *fin, FILE *fout)
     int c = 1;
     short root = hdt->nodes_number - 1, position = root;
     int8_t bits_read = 0;
-    unsigned char ch = (unsigned char)c;
-    while ( 1 )
+    out_bytes_count = 0;
+    uint64_t i = position_in_buffer(hdt->nodes_number, sizeof(huffman_decode_node));
+    while(1)
     {
-        if(bits_read == 0)
+        if (bits_read == 0)
         {
-            c = fgetc(fin);
-            ch = (unsigned char)c;
+            c = in_buffer[i++];
+            if (i == in_bytes_count)
+            {
+                if (in_bytes_count != IN_BUFFER_SIZE)
+                {
+                    position = tree_search(hdt, (unsigned char *)(&in_buffer[i]), position, root, &bits_read);
+                    out_buffer[out_bytes_count++] = hdt->tree[position].symbol;
+                    // Need to avoid "\ No newline at end of file" when compare with "diff" utility
+                    //out_buffer[out_bytes_count++] = '\n';
+                    fwrite(out_buffer, sizeof(int8_t), out_bytes_count, fout);
+                    return;
+                }
+                in_bytes_count = fread(in_buffer, sizeof(int8_t), IN_BUFFER_SIZE, fin);
+                i = 0;
+            }
         }
-        if(c == EOF) break;
-        position = tree_search(hdt, &ch, position, root, &bits_read);
+        position = tree_search(hdt, (unsigned char *)(&c), position, root, &bits_read);
         if(hdt->tree[position].left == -1)
         {
-            fputc(hdt->tree[position].symbol, fout);
+            out_buffer[out_bytes_count++] = hdt->tree[position].symbol;
+            if (out_bytes_count == OUT_BUFFER_SIZE)
+            {
+                fwrite(out_buffer, sizeof(int8_t), OUT_BUFFER_SIZE, fout);
+                out_bytes_count = 0;
+            }
             position = root;
         }
     }
@@ -29,7 +47,7 @@ void decode(huffman_decode_tree *hdt, FILE *fin, FILE *fout)
 
 int16_t tree_search(huffman_decode_tree *hdt, unsigned char *code, int16_t pos, int16_t root, int8_t *bits_read)
 {
-    int16_t l = hdt->tree[pos].left, r = hdt->tree[pos].right;
+    int16_t l = hdt->tree[pos].left, r = hdt->tree[pos].right, descendent = r;
     if(l == -1) return pos;
     else if((*bits_read) == 8)
     {
@@ -40,14 +58,8 @@ int16_t tree_search(huffman_decode_tree *hdt, unsigned char *code, int16_t pos, 
     {
         (*bits_read)++;
         if((*code & (1 << 7)) == 0)
-        {
-            *code <<= 1;
-            return tree_search(hdt, code, l, root, bits_read);
-        }
-        else
-        {
-            *code <<= 1;
-            return tree_search(hdt, code, r, root, bits_read);
-        }
+            descendent = l;
+        *code <<= 1;
+        return tree_search(hdt, code, descendent, root, bits_read);
     }
 }
